951f149f6c574809b66161be86c7daff927eed29
Marina Calheira de Oliveira <marina.oliveira@aln.senaicimatec.edu.br>
2023-03-20 10:12:20 -0300

    Criação dos modelos

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import string
+import random
+
+def generate_unique_code():
+    lenght = 6
+
+    while True:
+
 
 # Create your models here.
+class Room(models.Model):
+    code = models.CharField(max_length=8, default='', unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.models.BooleanField(null=False, default=False)
+    votes_to_skip = models.IntegerField(null=False, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
